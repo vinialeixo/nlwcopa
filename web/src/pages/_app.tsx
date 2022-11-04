0532a94db04aad87bbdb96076ac428a529a4e8ed
vinialeixo <vi_aleixodias@hotmail.com>
2022-11-04 20:30:53 -0300

    second class

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,3 +1,7 @@
+//pagina global, melhor pagina para que todas as paginas sigam os estilos
+
+import "./style/global.css";
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
