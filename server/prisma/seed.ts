4ae66cb4dbb4e0379497456a2f4f413ccdf734e0
vinialeixo <vi_aleixodias@hotmail.com>
2022-11-05 11:09:52 -0300

    add mobile file

---
+++ server/prisma/seed.ts
@@ -0,0 +1,60 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: "Joao Felix",
+      email: "joao_felix@hotmail.com",
+      avatarUrl: "https://github.com/vinialeixo.png",
+    },
+  });
+
+  const pool = await prisma.pool.create({
+    data: {
+      title: "Example Pool",
+      code: "BOL12234",
+      ownerId: user.id,
+
+      participant: {
+        create: {
+          userId: user.id,
+        },
+      },
+    },
+  });
+
+  await prisma.game.create({
+    data: {
+      date: "2022-11-03T23:39:36.786Z",
+      firstTeamCountryCode: "DE",
+      secondTeamCountryCode: "BR",
+    },
+  });
+
+  await prisma.game.create({
+    data: {
+      date: "2022-11-03T23:39:36.786Z",
+      firstTeamCountryCode: "BR",
+      secondTeamCountryCode: "AR",
+
+      guesses: {
+        create: {
+          firstTeamPoints: 2,
+          secondTeamPoints: 1,
+
+          participant: {
+            connect: {
+              userId_poolId: {
+                userId: user.id,
+                poolId: pool.id,
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+}
+main();
